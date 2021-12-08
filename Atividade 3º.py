15a71b9355a0158f83faf8d05ff62afb659b04b8
Fabio Costa <fabiocosta860@gmail.com>
2021-12-08 17:10:55 -0300

    first commit

---
+++ Atividade 3º.py
@@ -0,0 +1,66 @@
+
+from os import system
+from time import sleep                        # biblioteca de system 
+
+
+Senha_Atual = 19212
+comando = True
+
+while (comando != True):
+
+    
+    system('cls')
+
+    print('\t._______________________________________.')
+    print('\t|    >>> Classificação de alunos <<     |')
+    print('\t|                                       |')
+    print('\t| Opção:                                |')
+    print('\t|                                       |')
+    print('\t| F - Feminino                          |')
+    print('\t| M - Masculino                         |')
+    print('\t|                                       |')
+    print('\t|_______________________________________|')
+
+    sexo = input('\nDigita a opção: ')
+    nome = str (input('Digita seu nome completo: '))
+    senha = input('Digita a senha: ')
+     
+    if (senha == Senha_Atual) or (comando == True):
+      
+
+#----------(fim de while)-------------------------------                                                         
+     
+         if (sexo == 'M') or (sexo == 'm'):                                          # se a codição for igual a (M) ou (m)
+
+             print('\nOlá {} você e do gênero Masculino ({})\n'.format(nome,sexo))   # imprimir na tela 
+
+         elif (sexo == 'F') or (sexo == 'f'):                                        # se a codição for igual a (M) ou (m)
+
+             print('\nOlá {} você e do gênero Feminino ({})\n'.format(nome,sexo))    # impimir na tela 
+         else:
+             print('\nSexo Inválido !')  
+         comando == False                                                   # se não sexo invalido 
+            
+              
+
+     
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Projeto-e-Atividade-em-Python
